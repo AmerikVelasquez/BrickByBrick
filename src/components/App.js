716e5b0b8e189b9c603fc688c5baa8b90145b31f
AmerikVelasquez <amerik.velasquez22@gmail.com>
2022-03-08 12:43:10 -0800

    set up firebase

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+       <AuthProvider>
+       <Routes>
+         
+       </Routes>
+       </AuthProvider>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
