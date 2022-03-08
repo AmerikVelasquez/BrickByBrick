e169489216957db3ab285c39e85617d08358b2c0
AmerikVelasquez <amerik.velasquez22@gmail.com>
2022-03-08 15:42:59 -0800

    fix the firebaseconfig

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,17 +1,21 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
 
-const app = firebase.initializeApp({
-  REACT_APP_FIREBASE_API_KEY : process.env.REACT_APP_FIREBASE_API_KEY,
-REACT_APP_FIREBASE_AUTH_DOMAIN : process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-REACT_APP_FIREBASE_DATABASE_URL : process.env.REACT_APP_FIREBASE_DATABASE_URL,
-REACT_APP_FIREBASE_PROJECT_ID : process.env.REACT_APP_FIREBASE_PROJECT_ID,
-REACT_APP_FIREBASE_STORAGE_BUCKET : process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-REACT_APP_FIREBASE_MESSAGING_SENDER_ID : process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-REACT_APP_FIREBASE_APP_ID : process.env.REACT_APP_FIREBASE_APP_ID
+const firebaseConfig = {
+  apiKey : process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain : process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseUrl : process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId : process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket : process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId : process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId : process.env.REACT_APP_FIREBASE_APP_ID
 
 
-})
+}
 
-export const auth = app.auth()
-export default app
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {app, auth, db }
