9aa53723ef6aa04341e172a81dc14eb3f4ff09f8
AmerikVelasquez <amerik.velasquez22@gmail.com>
2022-03-08 19:12:26 -0800

    change signup function to login and move useEffect

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,37 +3,39 @@ import React, {useRef, useState, useEffect} from 'react'
 import { useAuth } from '../AuthContext';
 import { useNavigate } from 'react-router-dom';
 
-const Signup = () => {
+const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const {signup, currentUser} = useAuth();
+  const {login, currentUser} = useAuth();
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate();
 
+  
+  
+  const confirmLogin = async() => {
+    try {
+      setError('')
+      setLoading(true)
+      await login(emailRef.current.value, passwordRef.current.value)
+    } catch(error) {
+      setError(error.message)
+    }
+    setLoading(false)
+  }
+  
   useEffect(() => {
     if(currentUser) {
       navigate("/dashboard")
     }
   })
   
-
-  const confirmLogin = async() => {
-    try {
-      setError('')
-      setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-    } catch {
-      setError('Failed to login')
-    }
-    setLoading(false)
-  }
-
   return (
     <>
       <Card>
         <Card.Body>
           <Form>
+            <p>{error}</p>
             <Form.Group id= "email">
               <Form.Label>email</Form.Label>
               <Form.Control type = "email" ref= {emailRef}></Form.Control>
