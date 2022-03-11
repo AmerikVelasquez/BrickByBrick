ef0b6c4bd1ed3abe110b6e6394e5ee06c69bacd5
AmerikVelasquez <amerik.velasquez22@gmail.com>
2022-03-11 15:24:21 -0800

    reformat to show quiz conditionally

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,38 +4,41 @@ import { useNavigate } from "react-router-dom";
 import { auth, db, logout } from "./firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
 import { Button, Card} from 'react-bootstrap';
+import Quiz from './Quiz';
 
 function Dashboard() {
   const { currentUser , logout} = useAuth();
-  const {user, setUser} = useState();
   const navigate = useNavigate();
-  // const {completedQuiz, setQuiz} = useState();
+  const [quizForm, setQuizForm] = useState(false);
 
   const confirmLogout = () => {
     logout();
     navigate("/");
   }
 
-  // const startQuiz = () => {
-  //   if(completeQuiz){
-  //     <h1>"You've already done the quiz" </h1>
-  //   }else {
-  //     setQuiz(true)
-  //   }
-  // }
-
-  return (
-  <>
+  const startQuiz = () => {
+    setQuizForm(true);
+  }
+  
+  if(quizForm=== true){
+    return (
+      <Quiz 
+      quizControl = {setQuizForm} />
+    )
+  }else {
+    return (
+      <>
+  <h1>Brick By Brick</h1>
   <Card>
     <Card.Body>
     <h1>Email: </h1> {currentUser.email}
     </Card.Body>
   </Card>
   <Button onClick = {confirmLogout}>Log out</Button>
-  {/* <Button onClick = {startQuiz}> Begin with quiz</Button> */}
+  <Button onClick = {startQuiz}> Begin with quiz</Button>
   </>
     )
-
+  }
 }
-
+  
 export default Dashboard;
