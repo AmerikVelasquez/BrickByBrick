77107d3eca3dad16227b8cf6af6f10eb5272a651
AmerikVelasquez <amerik.velasquez22@gmail.com>
2022-03-13 12:08:15 -0700

    add finishing touches

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,16 @@
 import React ,{useState} from 'react';
 import {Form, Button, Dropdown, DropdownButton} from "react-bootstrap";
 import Brick from './Navbar';
+// import {getLego} from './Fetch';
 
 const Quiz = (props) => {
   const setFormVisible = props.quizControl;
+  const [value, setValue]= useState('');
+
+  const handleSelect=(e)=>{
+    console.log(value);
+    setValue(event.target.value);
+  }
 
   const quizFormSubmit = (event) => {
     event.preventDefault();
@@ -18,37 +25,37 @@ const Quiz = (props) => {
    <Form >
      <Form.Group>
      <Form.Label>Question 1. How long is your attention span for a specific project?(approx. hrs)</Form.Label>
-     <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 1">
-          <Dropdown.Item href="#/action-1">1</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">3</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">5</Dropdown.Item>
+     <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 1" onSelect={handleSelect}>
+          <Dropdown.Item eventKey="1" >1</Dropdown.Item>
+          <Dropdown.Item eventKey="2">3</Dropdown.Item>
+          <Dropdown.Item eventKey="3">5</Dropdown.Item>
           </DropdownButton>
      </Form.Group>
      <br />
      <Form.Group>
      <Form.Label>Question 2. What is your preferred theme?</Form.Label>
-     <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 2">
-          <Dropdown.Item href="#/action-1">Star Wars</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Architecture</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Vehicles</Dropdown.Item>
-          <Dropdown.Item href="#/action-4">Harry Potter</Dropdown.Item>
-          <Dropdown.Item href="#/action-5">Heroes and Villians</Dropdown.Item>
+     <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 2" onSelect={handleSelect}>
+          <Dropdown.Item >Star Wars</Dropdown.Item>
+          <Dropdown.Item >Architecture</Dropdown.Item>
+          <Dropdown.Item >Vehicles</Dropdown.Item>
+          <Dropdown.Item >Harry Potter</Dropdown.Item>
+          <Dropdown.Item >Heroes and Villians</Dropdown.Item>
           </DropdownButton>
      </Form.Group>
      <br />
      <Form.Group>
      <Form.Label>Question 3. Are you comfortable doing the project in parts?</Form.Label>
-     <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 3">
-          <Dropdown.Item href="#/action-1">Yes</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">No</Dropdown.Item>
+     <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 3" onSelect={handleSelect}>
+          <Dropdown.Item>Yes</Dropdown.Item>
+          <Dropdown.Item>No</Dropdown.Item>
           </DropdownButton>
      </Form.Group>
      <br />
      <Form.Group>
      <Form.Label>Question 4. Adult enjoyment or Children enjoyment? </Form.Label>
-      <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 4">
-          <Dropdown.Item href="#/action-1">Adult</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Children</Dropdown.Item>
+      <DropdownButton variant="danger" id="dropdown-basic-button" title="Answer 4" onSelect={handleSelect}>
+          <Dropdown.Item >Adult</Dropdown.Item>
+          <Dropdown.Item >Children</Dropdown.Item>
       </DropdownButton>
      </Form.Group>
      <br />
